2bf564665ee237966ff9e1113467e1e2f715ab72
Vicky <vyue013@gmail.com>
2019-07-18 12:05:33 -0700

    added iframe video condition

--- src/App.js
+++ src/App.js
@@ -7,21 +7,32 @@ import "./App.css";
 
 function App() {
 
-  const [data, setData] = useState({});
+  const [data, setData] = useState(null);
   useEffect(() => {
     console.log("first render");
-    axios.get("https://api.nasa.gov/planetary/apod?api_key=U8O9IcrQdFp0LuZWNWXx8aQSPIWxxEdRjpR9cd2W&date=2019-07-15")
-    .then(res => {
-      console.log(res.data);
-      setData(res.data);
-    });
+    axios.get("https://api.nasa.gov/planetary/apod?api_key=U8O9IcrQdFp0LuZWNWXx8aQSPIWxxEdRjpR9cd2W")
+      .then(res => {
+        console.log(res.data);
+        setData(res.data);
+      });
   }, [])
 
   return (
     <div className="App">
-      <PhotoCard title={data.title} 
-        url={data.url} 
-        explanation={data.explanation} />
+
+      {/* Checks if data is valid/true before rendering */}
+      {/* {data && <PhotoCard title={data.title}
+        url={data.url}
+        explanation={data.explanation} />} */}
+
+      {data ? <PhotoCard title={data.title}
+        url={data.url}
+        explanation={data.explanation}
+        date={data.date}
+        media={data.media_type}
+        /> 
+        : <div>Loading...</div> }
+
     </div>
   );
 }
--- src/components/PhotoCard.js
+++ src/components/PhotoCard.js
@@ -2,11 +2,18 @@ import React from "react";
 
 function PhotoCard(props) {
     console.log(props);
+
+    const media = props.media;
+
     return (
         <div>
             <h2>{props.title}</h2>
-            <img src={props.url} style={{ maxWidth: "50%" }} />
-            <p>
+
+            { media === "image" ? <img src={props.url} style={{ maxWidth: "50%" }} />
+            : <iframe src={props.url} width="500" height="300"></iframe>
+            }
+            <div>{props.date}</div>
+            <p style={{ margin: "auto", width: "80%" }}>
                 {props.explanation}
             </p>
         </div>
